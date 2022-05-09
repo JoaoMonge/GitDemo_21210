3cf3a15bf236692e056a2230bbdd4796f39bc251
Pedrospr <105205585+Pedrospr@users.noreply.github.com>
2022-05-09 10:43:19 +0100

    Escrevi o meu nome em nova linha

--- GitDemo_21210/Program.cs
+++ GitDemo_21210/Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, World! 21210 ");
-Console.WriteLine("Pedro Rocha ");
+Console.WriteLine("Pedro_Rocha ");
